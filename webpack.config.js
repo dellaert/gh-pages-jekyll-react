72a772e5cb3a22ba294923957e16feb2ca420b2c
Frank dellaert <dellaert@gmail.com>
2020-08-03 19:27:06 -0400

    Fixed webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+// import path from 'path';
+
 module.exports = {
   // webpack folder's entry js - excluded from jekll's build process.
-  entry: "./webpack/entry.js",
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'entry.jsx'),
   output: {
     // we're going to put the generated file in the assets folder so jekyll will grab it.
-      path: 'assets/javascripts/',
-      filename: "bundle.js"
+    path: path.resolve(__dirname, 'assets/javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
-    loaders: [
+    rules: [{
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
+        test: /\.jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env',
+              '@babel/preset-react', {
+                'plugins': ['@babel/plugin-proposal-class-properties']
+              }
+            ]
+          }
         }
       }
-    ]
+    ],
   }
 };
