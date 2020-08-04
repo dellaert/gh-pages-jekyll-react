541fa341e9d8a7644a0a9ff4001dc4dbf2c7fdd5
Varun Agrawal <varagrawal@gmail.com>
2020-08-04 13:07:40 -0500

    undo back to entry.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   // webpack folder's entry js - excluded from jekll's build process.
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'entry.jsx'),
   output: {
     // we're going to put the generated file in the assets folder so jekyll will grab it.
     path: path.resolve(__dirname, 'assets/js'),
