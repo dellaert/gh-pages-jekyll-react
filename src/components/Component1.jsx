aea99178ada59fc56a2523286264d60eb8621640
Frank Dellaert <dellaert@gmail.com>
2020-08-04 00:42:27 -0400

    Switched to stateless components

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -3,11 +3,8 @@ import React from 'react';
 
 type Props = {}
 
-class Component1 extends React.Component<Props> {
-  render() {
-    return (
-      <div>This is React component 1.</div>
-    )
-  }
+function Component1(props: Props) {
+  return <div>This is React component 1.</div>;
 }
+
 export default Component1;
--- src/components/Component2.jsx
+++ src/components/Component2.jsx
@@ -3,11 +3,8 @@ import React from 'react';
 
 type Props = {}
 
-class Component2 extends React.Component<Props> {
-  render() {
-    return (
-      <div>This is React component 2.</div>
-    )
-  }
+function Component2(props: Props) {
+  return <div>This is React component 2.</div>;
 }
+
 export default Component2;
